2c339ead678941b199b42eee7d714643adb685d0
Eray Uzgoren <uzgoren@metu.edu.tr>
2016-06-14 09:22:45 +0300

    No changes

--- include/grid2/Search_grid.hpp
+++ include/grid2/Search_grid.hpp
@@ -1,16 +1,24 @@
 #ifndef SEARCHGRID
 #define SEARCHGRID
 
+
+double phi(shared_ptr<Var> &q, Vec3 x, int_8 i0=0) {
+  auto n = searchVertexbyCoords(x, i0); 
+  if (n >= 0) listVertex[n]->evalPhi(q, &x); 
+} 
+
+
+
 int_8 searchVertexbyCoords(Vec3 a, int_8 i0=0) {
+  //cout << " Search in " << endl; 
   if (i0 >= listVertex.size() && i0 < 0) {
     cout << "Initial vertex do not exists" << endl; 
     exit(-1); 
   }
   auto v = listVertex[i0]; 
-  shared_ptr<Vertex >* tmp; 
   while (v) {            
     bool isFound = false; 
-    auto rc = (*v-a).abs();  auto rmin = rc; 
+    auto rc = (*v-a).abs(); auto rmin = rc; 
     if (v->cell.size() == 4) {
       for (auto ci : v->cell) {
 	if (ci < 0) continue; 
@@ -24,7 +32,8 @@ int_8 searchVertexbyCoords(Vec3 a, int_8 i0=0) {
       if (!isFound) break; 
     }     
   }
-  if (v) return v->id;     
+  //cout << " Search out " << *v << endl; 
+  if (v) return v->id; 
   else return -1; 
 }
 
@@ -33,13 +42,45 @@ int_8 searchCellbyCoords(Vec3 a, int_8 i0=0, bool isVertex=false) {
   auto vi = searchVertexbyCoords(a, j0); 
   if (vi < 0) return -1; 
   auto v = listVertex[vi]; 
-  auto xhat = interp.findXhat(a, v->xcoef, v->ycoef, v->zcoef); 
-  if (xhat[0] <= 0.5 && xhat[1] <= 0.5) return v->cell[0];
-  else if (xhat[0] > 0.5 && xhat[1] <= 0.5) return v->cell[1];
-  else if (xhat[0] > 0.5 && xhat[1] > 0.5) return v->cell[2]; 
-  else if (xhat[0] <= 0.5 && xhat[1] > 0.5) return v->cell[3];
-  else return v->cell[0]; 
+  
+  int_8 vc = -1; int_8 bc = 0; 
+  for (auto ic = 0; ic < v->cell.size(); ++ic) {
+    if (v->cell[ic] < 0) {bc = v->cell[ic]; continue;}
+    // Not correct for curvilinear //
+    auto xmin = Vec3(*listVertex[listCell[v->cell[ic]]->node[0]]);
+    auto xmax = Vec3(*listVertex[listCell[v->cell[ic]]->node[2]]);
+    if (a[0] < xmin[0]) continue; 
+    if (a[1] < xmin[1]) continue; 
+    if (a[0] > xmax[0]) continue; 
+    if (a[1] > xmax[1]) continue; 
+    vc = v->cell[ic]; 
+  }
+  if (vc >= 0) return vc; 
+  else if (bc < 0) return bc; 
+  else {
+    cout << " Cell not found! vc = " << vc; 
+    cout << " vi = " << vi << " a="<< a<< endl;
+    exit(-1); 
+  }
+  // auto xhat = int3D.findXhat(a, v->xcoef, v->ycoef, v->zcoef); 
+  // if (xhat[0] <= 0.5 && xhat[1] <= 0.5) return v->cell[0];
+  // else if (xhat[0] > 0.5 && xhat[1] <= 0.5) return v->cell[1];
+  // else if (xhat[0] > 0.5 && xhat[1] > 0.5) return v->cell[2]; 
+  // else if (xhat[0] <= 0.5 && xhat[1] > 0.5) return v->cell[3];
+  // else return -1; 
 }
+
+// int_8 searchInterpVertexbyCoords(Vec3 a, int_8 i0=0) {
+//   auto vi = searchVertexbyCoords(a, i0); 
+//   if (vi < 0) return -1; 
+//   auto v = listVertex[vi]; 
+//   auto xhat = int3D.findXhat(a, v->xcoef, v->ycoef, v->zcoef); 
+//   if (xhat[0] <= 0.5 && xhat[1] <= 0.5) return v->cell[0];
+//   else if (xhat[0] > 0.5 && xhat[1] <= 0.5) return v->cell[1];
+//   else if (xhat[0] > 0.5 && xhat[1] > 0.5) return v->cell[2]; 
+//   else if (xhat[0] <= 0.5 && xhat[1] > 0.5) return v->cell[3];
+//   else return v->cell[0]; 
+// }
 
 
 #endif
